f38aa0f26fef0352e57e3bd6753611104d502a37
Trong Nghia Nguyen <nghia.nguyen@ses-imagotag.com>
2023-02-19 16:19:48 +0100

    Add button

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -1,10 +1,10 @@
-import { Button } from "ui";
+import { Button } from "@crustnft/ui-standard";
 
 export default function Docs() {
   return (
     <div>
       <h1>Docs</h1>
-      <Button />
+      <Button>Ba</Button>
     </div>
   );
 }
